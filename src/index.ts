2290a87d885a94848077e17e26e38b6aaf0a6213
Enzo Viry <enzo.viry@gmail.com>
2026-07-02 22:02:40 +0200

    rien a branler

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 // Point d'entrée. Dans l'entrypoint on importe explicitement (plutôt que de se
 // fier aux globaux auto-importés) pour garantir l'ordre : valider la config →
 // jouer les migrations → connecter le bot.
-import * as djs from "discord.js";
-import { GlyriaClient, logger } from "@glyria/bot";
 import { runMigrations } from "./db/migrator.js";
-import { useConfig } from "./composables/useConfig.js";
 
 const config = useConfig();
 
